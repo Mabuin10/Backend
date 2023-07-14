f79ebd92fab3c49c9ff862ac9fac607623b4f066
Matias Abuin Capurro <mabuin5@gmail.com>
2023-07-13 21:03:29 -0300

    Making DesafioEntregable4

---
+++ DesafioEntregable4/src/app.js
@@ -0,0 +1,68 @@
+import express from "express";
+import productRouter from "./router/Products.router.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
+
+app.use("/api/products", productRouter);
+
+app.listen(PORT, () => {
+  console.log("servidor esta running en el puerto" + PORT);
+});
+
+
+// import express from "express";
+// import { ProductManager } from "./classes/ProductManager.js";
+
+// const app = express()
+// const PORT = 8080;
+// const productManager = new ProductManager("CelPhones.json")
+
+// let productos = [];
+
+// app.get("/", (req,res)=> {
+//     res.send("Hola Mundo")
+// });
+
+// app.get("/products", async (req,res) => {
+//     const { limit } = req.query;
+
+//     try{
+//         let response = await productManager.getProducts();
+//         if (limit) {
+//             let tempArray = response.filter((dat, index) => index < limit);
+//             // let tempArray = response.map((dat, index) => {
+//             //     return index < limit && dat;
+//             //   });
+//             res.json({ data: tempArray, limit: limit, quantity: tempArray.length });
+//         } else {
+//           res.json({ data: response, limit: false, quantity: response.length });
+//         }
+//     } catch(err) {
+//         console.log(err)
+//     }
+// })
+
+// app.get("/products/:pid", async (req, res) => {
+//     const { pid } = req.params;
+  
+//     let product = await productManager.getProductsById(pid);
+//     if (product) {
+//       res.json({ message: "Success", data: product });
+//     } else {
+//       res.json({
+//         message: "The product doesn`t exist",
+//       });
+//     }
+//   });
+
+
+// app.listen(PORT, () => {
+//     console.log("serv is running in port" + PORT);
+// })
